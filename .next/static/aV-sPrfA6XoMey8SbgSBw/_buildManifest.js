33b22db5ba3808676743543ee3e6db3efc437f99
Michael Dreesen <michaeldreesen90@gmail.com>
2023-02-11 22:49:24 -0700

    Adding sharp and PROD build

---
+++ .next/static/aV-sPrfA6XoMey8SbgSBw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/51afc172fa8a5e9e.css","static/chunks/pages/index-aa4b524d0b11b04b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
