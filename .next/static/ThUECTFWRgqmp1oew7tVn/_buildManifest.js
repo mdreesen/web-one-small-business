b49b3b0f9153ad20a7365afaad17308430a29e10
Michael Dreesen <michaeldreesen90@gmail.com>
2023-02-11 22:35:40 -0700

    Adding mobile nav link color to black

--- .next/static/ThUECTFWRgqmp1oew7tVn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/51afc172fa8a5e9e.css","static/chunks/pages/index-aa4b524d0b11b04b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
