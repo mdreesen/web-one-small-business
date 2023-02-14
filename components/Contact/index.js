7edaccc8013f407085a9d82fd79e8fff07b78c0d
Michael Dreesen <michaeldreesen90@gmail.com>
2023-02-13 20:10:21 -0700

    Adding bootstrap form

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,43 +1,34 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 import styles from '../../styles/Contact.module.css';
-// import emailjs from 'emailjs-com';
-
-// importing email.js
-// import{ init } from 'emailjs-com';
-// init("user_5PifaWCGrPQuL7ghKo7R5");
-
-// import './ContactUs.css';
 
 export default function Contact() {
 
-    function sendEmail(e) {
-        e.preventDefault();
-
-        // emailjs.sendForm('service_u2qbro4', 'template_14mdvws', e.target, 'user_5PifaWCGrPQuL7ghKo7R5')
-        //   .then((result) => {
-        //       console.log(result.text);
-        //   }, (error) => {
-        //       console.log(error.text);
-        //   });
-    }
-
     return (
-        <div className={styles["contact-section"]}>
-                <form className={styles["contactForm"]} onSubmit={sendEmail}>
-                    <input type="hidden" name="contact_number" />
-                    <div>
-                        <label>Name</label><br />
-                        <input type="text" name="user_name" placeholder='Required' />
-                    </div>
-                    <div>
-                        <label>Email</label><br />
-                        <input type="email" name="user_email" placeholder='Required' />
-                    </div>
-                    <div>
-                        <label>Message</label><br />
-                        <textarea name="message" placeholder='Required' />
-                    </div>
-                    <input className={styles["submit-btn"]} type="submit" value="Send" />
-                </form>
-        </div>
+        <Form action="https://formsubmit.co/?" method="POST">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="name" placeholder="Enter name" />
+                <Form.Text className="text-muted">
+                    First and last name
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 }
+
--- components/Hero/index.js
+++ components/Hero/index.js
@@ -4,19 +4,22 @@ import styles from '../../styles/Hero.module.css';
 
 export default function Hero(props) {
     return (
-        <Parallax className={styles['heroImage']} bgImage={`/images/${props.heroImage}.webp`} strength={180}>
-            <h1 className={styles['content']}>
-                <Image
-                    className={styles['secondaryImage']}
-                    src="/images/logo_inhanced_noBackground.webp"
-                    alt="Mission Mountains handy work of bedroom"
-                    position="relative"
-                    width={444}
-                    height={216}
-                />
-                QUALITY-DRIVEN CONSTRUCTION.<br />
-                CLIENT-TAILORED EXPERIENCES.
-            </h1>
-        </Parallax>
+        <div>
+        {/* <div className={styles['container']} /> */}
+            <Parallax className={styles['heroImage']} bgImage={`/images/${props.heroImage}.webp`} strength={180}>
+                <h1 className={styles['content']}>
+                    <Image
+                        className={styles['secondaryImage']}
+                        src="/images/logo_inhanced_noBackground.webp"
+                        alt="Mission Mountains handy work of bedroom"
+                        position="relative"
+                        width={444}
+                        height={216}
+                    />
+                    QUALITY-DRIVEN CONSTRUCTION.<br />
+                    CLIENT-TAILORED EXPERIENCES.
+                </h1>
+            </Parallax>
+        </div>
     )
 }
